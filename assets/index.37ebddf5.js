50867118924b58849c62307ee69527d2c2f01d3c
Yourenz <31525716+Yourenz@users.noreply.github.com>
2021-06-05 04:37:42 +0000

    Deploying to gh-pages from @ Yourenz/react-vite-admin@423c32b9b8194ba3a57cc1cc8d2bd3414b1aaad8 🚀

---
+++ assets/index.37ebddf5.js
@@ -0,0 +1,1 @@
+import{l as e}from"./vendor.74592872.js";export default()=>e.createElement("div",null,"DashBoard");
--- assets/index.bb2c39ff.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./vendor.c38349bd.js";export default()=>e.createElement("div",null,"DashBoard");
