0743e4d4c411a28e1950833ee6f690cc8b096ae9
yourenz <yourenz_work@qq.com>
2021-06-18 14:48:00 +0800

    style: add transition

--- src/layout/components/Container/index.tsx
+++ src/layout/components/Container/index.tsx
@@ -1,12 +1,17 @@
 import { Layout } from 'antd'
 
+import { useAppSelector } from '@/store'
+
 import Main from '../Main'
 import SideBar from '../SiderBar'
 
 const Container = (): JSX.Element => {
+  const { collapsed } = useAppSelector((state) => state.layoutReducer)
+
   return (
     <Layout style={{ height: '100%' }}>
       <SideBar />
+      <div style={{ width: collapsed ? 80 : 200, transition: 'width 0.2s' }} />
       <Main />
     </Layout>
   )
